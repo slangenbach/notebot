013563ec34e3e59fb8e8156346c808fc4566d035
Stefan Langenbach <stefan@langenbach.online>
2023-08-01 20:45:48 +0000

    Convert notebook into app

--- notebot/constants.py
+++ notebot/constants.py
@@ -1,9 +1,10 @@
 """Package wide constants."""
 from pathlib import Path
+from typing import Final
 
-ROOT_PATH = Path(__file__).parent.parent
-DATA_PATH = ROOT_PATH / "data"
-NOTES_PATH = DATA_PATH / "notes"
-DB_PATH = DATA_PATH / "db"
+ROOT_PATH: Final = Path(__file__).parent.parent
+DATA_PATH: Final = ROOT_PATH / "data"
+NOTES_PATH: Final = DATA_PATH / "notes"
+DB_PATH: Final = DATA_PATH / "db"
 
-NOTE_REPO_URL = "https://github.com/slangenbach/notes.git"
+NOTE_REPO_URL: Final = "https://github.com/slangenbach/notes.git"
---
+++ notebot/main.py
@@ -0,0 +1,124 @@
+"""Main entry point for NoteBot."""
+
+import argparse
+from typing import Final
+
+import gradio as gr
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import GitLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import MarkdownTextSplitter
+from langchain.vectorstores import FAISS
+
+from notebot.constants import DB_PATH, NOTE_REPO_URL, NOTES_PATH
+
+load_dotenv()
+
+EMBEDDINGS: Final = OpenAIEmbeddings()  # pyright: ignore
+
+
+class NoteBot:
+    """Base class for interacting with NoteBot."""
+
+    def __init__(self, chain) -> None:
+        self.chain = chain
+
+    def get_response(self, question: str) -> dict:
+        return self.chain({"question": question})
+
+    def chat(self, message: str, history: list) -> str:
+        response = self.get_response(question=message)
+
+        return response["answer"]
+
+
+def filter_notes(file_path: str) -> bool:
+    """
+    Filter out none markdown notes and READMEs.
+
+    Args:
+        file_path (str): Path to file
+
+    Returns:
+        bool: Whether to filter out a note
+    """
+    return file_path.endswith(".md") and not file_path.endswith("README.md")
+
+
+def load_and_ingest_notes(note_repo_url: str) -> None:
+    """
+    Load markdown notes from Git repo and ingest them into vector store.
+
+    Args:
+        note_repo_url (str): URL of Git repo to load notes from
+    """
+    loader = GitLoader(
+        repo_path=str(NOTES_PATH),
+        clone_url=note_repo_url,
+        file_filter=filter_notes,
+    )
+    splitter = MarkdownTextSplitter()
+    docs = loader.load_and_split(text_splitter=splitter)
+    db = FAISS.from_documents(documents=docs, embedding=EMBEDDINGS)
+    db.save_local(folder_path=str(DB_PATH))
+
+
+def get_chain(db):
+    """
+    Get LangChain to interact with notes.
+
+    Args:
+        db: Vector store
+
+    Returns:
+        LangChain chain
+    """
+    llm = ChatOpenAI(temperature=0)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, retriever=db.as_retriever(), memory=memory
+    )
+
+    return chain
+
+
+def main(args):
+    """
+    Launch NoteBot.
+
+    Args:
+        args (_type_): CLI arguments
+    """
+    load_dotenv()
+
+    if not NOTES_PATH.exists() and not DB_PATH.exists():
+        load_and_ingest_notes(note_repo_url=args.note_repo_url)
+
+    db = FAISS.load_local(folder_path=str(DB_PATH), embeddings=EMBEDDINGS)
+    chain = get_chain(db=db)
+    notebot = NoteBot(chain=chain)
+
+    ui = gr.ChatInterface(
+        fn=notebot.chat,
+        title="NoteBot",
+        examples=[
+            "List the title of all notes I can ask you about",
+            "Generate a brief summary of my LangChain notes",
+            "Do I have some notes related to COBOL?",
+        ],
+        cache_examples=True,
+    )
+    ui.launch()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Welcome to NoteBot")
+    parser.add_argument(
+        "--note-repo-url", help="URL of Git repo to load notes from", default=NOTE_REPO_URL
+    )
+    args = parser.parse_args()
+
+    main(args)
